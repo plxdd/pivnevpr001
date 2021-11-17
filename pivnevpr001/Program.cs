8ee828b96026430cc41073f75da4b6c9a9079188
stene <stene@PLXD>
2021-11-17 21:50:06 +0300

    Добавьте файлы проекта.

---
+++ pivnevpr001/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace pivnevpr001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("всем привет меня зовут влад а5 который пиво ахахаха(( и сегодня у нас будет 24 часа без сна челлендж!");
+            Console.WriteLine("как думаете кто победит?? ставьте класс если влад, коммент если влад");
+            Console.WriteLine("подписывайтесь на канал и включайте колокольчик чтобы не пропускать новые видео!!");
+            Console.WriteLine("@pineeapplevk");
+        }
+    }
+}
